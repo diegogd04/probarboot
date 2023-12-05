385694ea4d6f71a5723bd37d5caec9e0e111b215
diegogd04 <diego2612004@gmail.com>
2023-12-05 19:52:45 +0100

    Ejercicio-bootstrap-tabla-but

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'probarboot';
+  datosFila1: String[] = ["PRIMERO", "SEGUNDO"]
+  datosFila2: String[] = ["TERCERO", "CUARTO"]
+  textos: String[] = []
+  buttonClick(number: number) {
+    switch (number) {
+      case 1:
+        this.textos[number] = "SOY LA PRIMERA CELDA"
+        break
+      case 2:
+        this.textos[number] = "SOY LA SEGUNDA CELDA"
+        break
+      case 3:
+        this.textos[number] = "SOY LA TERCERA CELDA"
+        break
+      case 4:
+        this.textos[number] = "SOY LA CUARTA CELDA"
+        break
+    }
+  }
 }
